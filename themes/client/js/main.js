7a59bde58378430add486967930d877d6eda504c
akoch-ov <ako40ff@gmail.com>
2017-01-03 22:52:02 +0300

    header-text for mobile

--- themes/client/js/main.js
+++ themes/client/js/main.js
@@ -5,4 +5,14 @@ jQuery(function($) {
 	$('.menu-button.cross').click(function(event) {
         $('body').removeClass('pushed');
 	});
+	
+	if ($(window).width() <= '979'){
+		$('header .logo').click(function(event) {
+			$('.header-text').toggle('fast');
+			return false;
+		});
+		$('header .header-text').click(function(event) {
+			$('.header-text').toggle('fast');
+		});
+	}
 });
