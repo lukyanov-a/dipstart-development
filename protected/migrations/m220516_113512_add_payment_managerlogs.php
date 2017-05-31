a577305a2a99bed65dac80ef3689bae87870fd10
22ialexandr <22ialexandr@gmail.com>
2017-05-31 21:12:17 +0300

    addon to salary(fix)

--- protected/migrations/m220516_113512_add_payment_managerlogs.php
+++ protected/migrations/m220516_113512_add_payment_managerlogs.php
@@ -4,7 +4,7 @@ class m220516_113512_add_payment_managerlogs extends CDbMigration
 {
     public function safeUp()
     {
-        $this->addColumn('ManagerLogs', 'payment', 'INT(11) NULL DEFAULT "0"');
+        $this->addColumn('ManagerLogs', 'payment', 'TINYINT(1) NULL DEFAULT "0"');
     }
 
     public function down()
